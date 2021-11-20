e8adda1ef0328561325b7dc3ac855244b1fec3c4
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-20 11:29:50 +0800

    Create fb.js

---
+++ fb.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+  
+  'https://www.effectivegatetocontent.com/z1wn7uchfh?key=8e7e9cee0a6e0c3072355923040687d0',
+  'https://www.effectivegatetocontent.com/mjica607za?key=2e7bf748dd3e7d803a90bbd32fd95f54'
+  
+ 
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
