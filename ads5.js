db6099dc7fdda19271cb301cff620b5b5735b40f
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-12-11 11:10:26 +0800

    Create ads5.js

---
+++ ads5.js
@@ -0,0 +1,10 @@
+var dataUrl = [
+  
+  'https://www.effectivegatetocontent.com/m6dshe0gb?key=6282fcaa1838cde42864f768f18a8723',
+  'https://www.effectivegatetocontent.com/g1mt10eak?key=6601e332ac1abfbc3d0cb6070b4b917a',
+  'https://www.effectivegatetocontent.com/qk9frn3aqu?key=1ba1f97641cdb9a5cd30010a176e827c'
+  
+ 
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
