f2a3bbaade21400ceb077a4e81c213524f514372
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-12-15 19:57:36 +0800

    Create Adsterra3.js

---
+++ Adsterra3.js
@@ -0,0 +1,15 @@
+var dataUrl = [
+  
+  'https://arousedimitateplane.com/tykpbwuirx?key=2bd2cc016f846e31eba9c90b01a4ed2d',
+  'https://arousedimitateplane.com/hd8pzzdktj?key=ed4ea6cf3942fb996e79906e1c59a64d',
+  'https://arousedimitateplane.com/p1e7i05y?key=70d51c7afe95955a0dc6c6982c478b84',
+  'https://www.effectivegatetocontent.com/bk6xziud?key=24a93215ae3fd40b02e4035bb344aa7b',
+  'https://www.effectivegatetocontent.com/wbbzz1u91r?key=d0ccda3580917dc6f6247f5013028171'
+  
+  
+  
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
+ad
