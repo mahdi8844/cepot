be992fd731fb41a55a2437fd741aa0549054d080
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-12-17 23:08:52 +0800

    Create amat.js

---
+++ amat.js
@@ -0,0 +1,13 @@
+var dataUrl = [
+  
+  'https://www.effectivegatetocontent.com/jg8vjg3xwy?key=edd04c0b0bd41c0bd8494c2890390ad4',
+  'https://www.effectivegatetocontent.com/xyhh9ebt?key=70bf1cf20c9c44d1a9b9079b1159855b'
+
+  
+  
+  
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
+ad
