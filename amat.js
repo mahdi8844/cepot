831fa84becb07d6741eb7483a01fee2b25920c22
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-12-18 21:47:32 +0800

    Update amat.js

--- amat.js
+++ amat.js
@@ -1,7 +1,8 @@
 var dataUrl = [
   
   'https://www.effectivegatetocontent.com/jg8vjg3xwy?key=edd04c0b0bd41c0bd8494c2890390ad4',
-  'https://www.effectivegatetocontent.com/xyhh9ebt?key=70bf1cf20c9c44d1a9b9079b1159855b'
+  'https://www.effectivegatetocontent.com/xyhh9ebt?key=70bf1cf20c9c44d1a9b9079b1159855b',
+  'https://www.effectivegatetocontent.com/mmj2k7nrdt?key=a9adec45b7e8413e8085337ca17f76bf'
 
   
   
