5511de01182ade686a29314db00c46d68371eb24
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-01-01 16:17:56 +0800

    Create adspop.js

---
+++ adspop.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+  
+  'https://www.trustedcpmrevenue.com/zn3yk2vznm?key=7727f8a5696dddff1ee488a05d571ab4'
+  
+  
+ 
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
