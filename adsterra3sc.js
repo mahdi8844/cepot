bb8c95b38791b39c8693b62195929bb81b5bcaa8
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-01-08 00:52:16 +0800

    Update adsterra3sc.js

--- adsterra3sc.js
+++ adsterra3sc.js
@@ -3,9 +3,9 @@ var dataUrl = [
   'https://arousedimitateplane.com/tykpbwuirx?key=2bd2cc016f846e31eba9c90b01a4ed2d',
   'https://arousedimitateplane.com/hd8pzzdktj?key=ed4ea6cf3942fb996e79906e1c59a64d',
   'https://arousedimitateplane.com/p1e7i05y?key=70d51c7afe95955a0dc6c6982c478b84',
-  'https://www.trustedcpmrevenue.com/m6dshe0gb?key=6282fcaa1838cde42864f768f18a8723',
-  'https://www.trustedcpmrevenue.com/g1mt10eak?key=6601e332ac1abfbc3d0cb6070b4b917a',
-  'https://www.trustedcpmrevenue.com/qk9frn3aqu?key=1ba1f97641cdb9a5cd30010a176e827c'
+  'https://annoynoveltyeel.com/s18bncwz?key=d185a86de0729a4f4e0943ca13d6d8ee',
+  'https://annoynoveltyeel.com/bk6xziud?key=24a93215ae3fd40b02e4035bb344aa7b',
+  'https://annoynoveltyeel.com/wbbzz1u91r?key=d0ccda3580917dc6f6247f5013028171'
   
   
   
