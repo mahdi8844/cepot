4c551d8bdc8dbd5261ac725872554fea1eebdaa9
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-01-07 10:26:07 +0800

    Create adsterra3sc.js

---
+++ adsterra3sc.js
@@ -0,0 +1,16 @@
+var dataUrl = [
+  
+  'https://arousedimitateplane.com/tykpbwuirx?key=2bd2cc016f846e31eba9c90b01a4ed2d',
+  'https://arousedimitateplane.com/hd8pzzdktj?key=ed4ea6cf3942fb996e79906e1c59a64d',
+  'https://arousedimitateplane.com/p1e7i05y?key=70d51c7afe95955a0dc6c6982c478b84',
+  'https://www.trustedcpmrevenue.com/m6dshe0gb?key=6282fcaa1838cde42864f768f18a8723',
+  'https://www.trustedcpmrevenue.com/g1mt10eak?key=6601e332ac1abfbc3d0cb6070b4b917a',
+  'https://www.trustedcpmrevenue.com/qk9frn3aqu?key=1ba1f97641cdb9a5cd30010a176e827c'
+  
+  
+  
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
+ad
