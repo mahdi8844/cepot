6bc6b89a7ce0676a938aebf542961edd34e18060
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-01-13 10:40:56 +0800

    Update adspop.js

--- adspop.js
+++ adspop.js
@@ -1,6 +1,7 @@
 var dataUrl = [
   
-  'https://www.trustedcpmrevenue.com/zn3yk2vznm?key=7727f8a5696dddff1ee488a05d571ab4'
+  'https://www.trustedcpmrevenue.com/zn3yk2vznm?key=7727f8a5696dddff1ee488a05d571ab4',
+  'https://www.trustedcpmrevenue.com/kxa617jh?key=43da07d0d666b5a1f4778b1beab504f4'
   
   
  
