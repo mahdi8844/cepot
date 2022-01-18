260835c0182b966361502e2f12f4b355cb19bee6
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-01-18 19:59:13 +0800

    Create ansari2.js

---
+++ ansari2.js
@@ -0,0 +1,16 @@
+var dataUrl = [
+  
+  'https://www.trustedcpmrevenue.com/j1xw0x0rcx?key=1c9adcc6bca3296fac552e8c1e5b0a82',
+  'https://www.trustedcpmrevenue.com/jpp55717e?key=8507cef1a397afc5fea1dc360c0886b3',
+  'https://www.trustedcpmrevenue.com/kdvahsb0wj?key=66f1e59b1b551fb24893ae895dc1d658',
+  'https://www.trustedcpmrevenue.com/nx1jfdzr?key=8a9b83430981254c00c2804ddb2b7d92'
+ 
+
+  
+  
+  
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
+ad
