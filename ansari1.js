64f831efd3b6aca08bdd0c48fa91b2500ed7cfea
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-01-18 12:53:35 +0800

    Update and rename ansari.js to ansari1.js

---
+++ ansari1.js
@@ -0,0 +1,16 @@
+var dataUrl = [
+  
+  'https://www.trustedcpmrevenue.com/j1rxj172?key=adc36aa55f17d2c41223d390f5d7be1e',
+  'https://www.trustedcpmrevenue.com/xyhh9ebt?key=70bf1cf20c9c44d1a9b9079b1159855b',
+  'https://www.trustedcpmrevenue.com/ftevmc9ffu?key=88b2c5bdae56a3d0c808da01c98c12d7',
+  'https://www.trustedcpmrevenue.com/zew272d3wv?key=08a55baa42028feb47832b052f7f041d'
+ 
+
+  
+  
+  
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
+ad
