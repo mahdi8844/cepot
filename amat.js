0730e13b1b6b598121a54f0cbba46656470de9f7
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-01-19 09:02:09 +0800

    Update amat.js

--- amat.js
+++ amat.js
@@ -1,9 +1,11 @@
 var dataUrl = [
   
-  'https://www.effectivegatetocontent.com/jg8vjg3xwy?key=edd04c0b0bd41c0bd8494c2890390ad4',
-  'https://www.effectivegatetocontent.com/xyhh9ebt?key=70bf1cf20c9c44d1a9b9079b1159855b',
-  'https://www.effectivegatetocontent.com/mmj2k7nrdt?key=a9adec45b7e8413e8085337ca17f76bf'
+  'https://grantedpigsunborn.com/jg8vjg3xwy?key=edd04c0b0bd41c0bd8494c2890390ad4',
+  'https://grantedpigsunborn.com/mmj2k7nrdt?key=a9adec45b7e8413e8085337ca17f76bf',
+  'https://www.trustedcpmrevenue.com/rupa7fbz60?key=217702c8703c64f1061f693abf05552b',
+  'https://www.trustedcpmrevenue.com/qbsdvqb3cs?key=0401ff573602f40f769ebb273e5b8410'
 
+ 
   
   
   
