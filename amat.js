93c745c77b03b6dded61a99e467481743440bac1
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-01-19 22:17:58 +0800

    Update amat.js

--- amat.js
+++ amat.js
@@ -1,10 +1,17 @@
 var dataUrl = [
   
-  'https://grantedpigsunborn.com/jg8vjg3xwy?key=edd04c0b0bd41c0bd8494c2890390ad4',
-  'https://grantedpigsunborn.com/mmj2k7nrdt?key=a9adec45b7e8413e8085337ca17f76bf',
-  'https://www.trustedcpmrevenue.com/rupa7fbz60?key=217702c8703c64f1061f693abf05552b',
-  'https://www.trustedcpmrevenue.com/qbsdvqb3cs?key=0401ff573602f40f769ebb273e5b8410'
-
+'https://grantedpigsunborn.com/jg8vjg3xwy?key=edd04c0b0bd41c0bd8494c2890390ad4',
+'https://grantedpigsunborn.com/mmj2k7nrdt?key=a9adec45b7e8413e8085337ca17f76bf',
+'https://grantedpigsunborn.com/feb7xbxrb?key=4e2587c12cf4b8990532d81ac580ce1f',
+'https://grantedpigsunborn.com/b9z3pexh7c?key=241a8547e9c0e7911d728d3ad6aac6b4',
+'https://grantedpigsunborn.com/eun6bc7z?key=7eb35efd04314c17bed2c3b7411852d9',
+'https://www.trustedcpmrevenue.com/qbsdvqb3cs?key=0401ff573602f40f769ebb273e5b8410',
+'https://www.trustedcpmrevenue.com/rupa7fbz60?key=217702c8703c64f1061f693abf05552b',
+'https://www.trustedcpmrevenue.com/yizdn4izw?key=978099cfa743973b069a4c422c8cc4fd',
+'https://www.trustedcpmrevenue.com/t4qf9rj77?key=00e43e1317637724820831972f1d8e90',
+'https://www.trustedcpmrevenue.com/y7t7umra?key=ddd07732f35569c3ec5ad25ea1134217',
+'https://www.trustedcpmrevenue.com/vmzwb7hn?key=b10e1f51e331e71e0021221d53a1f2de',
+'https://www.trustedcpmrevenue.com/gdfvgki14?key=e99f65f66709645dcb371ae7d80ec142'
  
   
   
