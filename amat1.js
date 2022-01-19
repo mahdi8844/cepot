ff9ab4a774b010f7b55ffea6f38057c0bfd3cdda
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-01-19 22:22:40 +0800

    Update amat1.js

--- amat1.js
+++ amat1.js
@@ -1,9 +1,17 @@
 var dataUrl = [
   
-  'https://www.trustedcpmrevenue.com/tiiae97v?key=a7b4d96df0730d539d59907332df677c',
-  'https://www.trustedcpmrevenue.com/bzp6w4f9?key=1c51dd0fff2207ac667bc42f8b0de48b',
-  'https://www.trustedcpmrevenue.com/vvbprkvhh?key=6bc5aca654bd28d125151eb699977a3f',
-  'https://www.trustedcpmrevenue.com/q2jmcuid?key=efefb2c32cebbff68b8da2bfee4449ea'
+'https://www.trustedcpmrevenue.com/bzp6w4f9?key=1c51dd0fff2207ac667bc42f8b0de48b',
+'https://www.trustedcpmrevenue.com/tiiae97v?key=a7b4d96df0730d539d59907332df677c',
+'https://www.trustedcpmrevenue.com/cdzn7cbm?key=b2aeba2037ff43b37374d1d9fcccc12c',
+'https://www.trustedcpmrevenue.com/r4bk0gyx2?key=976dfcfb39bde217438881457e852721',
+'https://www.trustedcpmrevenue.com/dnj3chpn?key=40b634377d7d3733568f5dc8a9d01695',
+'https://www.trustedcpmrevenue.com/kt48cu4q?key=ccc9c339ae2b4ff0886f4b788cdea7d3',
+'https://www.trustedcpmrevenue.com/nz91fa4v?key=1a2e7d9384e696bed678861159a8179a',
+'https://www.trustedcpmrevenue.com/q2jmcuid?key=efefb2c32cebbff68b8da2bfee4449ea',
+'https://www.trustedcpmrevenue.com/vvbprkvhh?key=6bc5aca654bd28d125151eb699977a3f',
+'https://www.trustedcpmrevenue.com/cjktwxbp?key=3f2fb5ea1dc2fc5723dcd315aeb06d3b',
+'https://www.trustedcpmrevenue.com/ipxi5iug?key=e203f6d1a7c4b2be29438ff91c5136cc',
+'https://www.trustedcpmrevenue.com/sfwfw2ft?key=fac1185360358dafcb583e43e8200c5b'
 
  
   
