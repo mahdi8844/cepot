59c31b4c1086d90a021c3acef6c5a414619226b5
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-01-25 10:35:26 +0800

    Update ansari2.js

--- ansari2.js
+++ ansari2.js
@@ -1,17 +1,17 @@
 var dataUrl = [
   
-  'https://www.trustedcpmrevenue.com/j1xw0x0rcx?key=1c9adcc6bca3296fac552e8c1e5b0a82',
-  'https://www.trustedcpmrevenue.com/jpp55717e?key=8507cef1a397afc5fea1dc360c0886b3',
-  'https://www.trustedcpmrevenue.com/efem9wbsk?key=b2380e7e2441eb8676f2bfad8134ee97',
-  'https://www.trustedcpmrevenue.com/i3c11jfyzq?key=1cd656c0804d50396e719a76a043e8e8',
-  'https://www.trustedcpmrevenue.com/up6wxiqm91?key=f665e3bb7e8a4bf4c6c8cb38f693c707',
-  'https://www.trustedcpmrevenue.com/hbuxurxw?key=86bc21eccc1a348f25d791b6d7ca18be',
-  'https://www.trustedcpmrevenue.com/kdvahsb0wj?key=66f1e59b1b551fb24893ae895dc1d658',
-  'https://www.trustedcpmrevenue.com/nx1jfdzr?key=8a9b83430981254c00c2804ddb2b7d92',
-  'https://www.trustedcpmrevenue.com/h8iv88se1v?key=21893a1ff00c4483973d3efc2febd04c',
-  'https://www.trustedcpmrevenue.com/rm5e0t7wd?key=d1c7d5a4bfef1f1a791c9f7c5cb38ec7',
-  'https://www.trustedcpmrevenue.com/yevb1vpg2?key=0127f56241a83ccd37506a3d59a797fe',
-  'https://www.trustedcpmrevenue.com/ge0by5fkp?key=88d8ebc23fa0d2911bc6c6b785d212db'
+  'https://angularconstitution.com/j1xw0x0rcx?key=1c9adcc6bca3296fac552e8c1e5b0a82',
+  'https://angularconstitution.com/jpp55717e?key=8507cef1a397afc5fea1dc360c0886b3',
+  'https://angularconstitution.com/efem9wbsk?key=b2380e7e2441eb8676f2bfad8134ee97',
+  'https://angularconstitution.com/i3c11jfyzq?key=1cd656c0804d50396e719a76a043e8e8',
+  'https://angularconstitution.com/up6wxiqm91?key=f665e3bb7e8a4bf4c6c8cb38f693c707',
+  'https://angularconstitution.com/hbuxurxw?key=86bc21eccc1a348f25d791b6d7ca18be',
+  'https://fireworkswad.com/kdvahsb0wj?key=66f1e59b1b551fb24893ae895dc1d658',
+  'https://fireworkswad.com/nx1jfdzr?key=8a9b83430981254c00c2804ddb2b7d92',
+  'https://fireworkswad.com/h8iv88se1v?key=21893a1ff00c4483973d3efc2febd04c',
+  'https://fireworkswad.com/rm5e0t7wd?key=d1c7d5a4bfef1f1a791c9f7c5cb38ec7',
+  'https://fireworkswad.com/yevb1vpg2?key=0127f56241a83ccd37506a3d59a797fe',
+  'https://fireworkswad.com/ge0by5fkp?key=88d8ebc23fa0d2911bc6c6b785d212db'
  
 
   
