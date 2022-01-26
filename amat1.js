6d1dfa72e5740095cff4bd1e625e77b1d0576615
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-01-26 14:38:34 +0800

    Update amat1.js

--- amat1.js
+++ amat1.js
@@ -1,10 +1,6 @@
 var dataUrl = [
   
-'https://fireworkswad.com/q2jmcuid?key=efefb2c32cebbff68b8da2bfee4449ea',
-'https://fireworkswad.com/vvbprkvhh?key=6bc5aca654bd28d125151eb699977a3f',
-'hhttps://fireworkswad.com/cjktwxbp?key=3f2fb5ea1dc2fc5723dcd315aeb06d3b',
-'https://fireworkswad.com/ipxi5iug?key=e203f6d1a7c4b2be29438ff91c5136cc',
-'https://fireworkswad.com/sfwfw2ft?key=fac1185360358dafcb583e43e8200c5b',
+
 'https://angularconstitution.com/bzp6w4f9?key=1c51dd0fff2207ac667bc42f8b0de48b',
 'https://angularconstitution.com/tiiae97v?key=a7b4d96df0730d539d59907332df677c',
 'https://angularconstitution.com/cdzn7cbm?key=b2aeba2037ff43b37374d1d9fcccc12c',
