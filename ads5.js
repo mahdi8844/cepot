a735cf0ac5442b8f476be5281e1f095535050234
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-01-29 10:41:29 +0800

    Update ads5.js

--- ads5.js
+++ ads5.js
@@ -1,8 +1,8 @@
 var dataUrl = [
   
-  'https://www.effectivegatetocontent.com/m6dshe0gb?key=6282fcaa1838cde42864f768f18a8723',
-  'https://www.effectivegatetocontent.com/g1mt10eak?key=6601e332ac1abfbc3d0cb6070b4b917a',
-  'https://www.effectivegatetocontent.com/qk9frn3aqu?key=1ba1f97641cdb9a5cd30010a176e827c'
+  'https://angularconstitution.com/qk9frn3aqu?key=1ba1f97641cdb9a5cd30010a176e827c',
+  'https://angularconstitution.com/g1mt10eak?key=6601e332ac1abfbc3d0cb6070b4b917a',
+  'https://angularconstitution.com/m6dshe0gb?key=6282fcaa1838cde42864f768f18a8723'
   
  
 ];
