7d6aa1aa4fb5b8f318c6946d3797f6447b505f66
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-02-01 09:40:22 +0800

    Update adsss4.js

--- adsss4.js
+++ adsss4.js
@@ -1,7 +1,7 @@
 var dataUrl = [
   
   'https://www.effectivegatetocontent.com/mjica607za?key=2e7bf748dd3e7d803a90bbd32fd95f54',
-  'https://www.effectivegatetocontent.com/i95fwhrt?key=58f137185091c64206b110b0aa36ebdf'
+  'https://angularconstitution.com/g1mt10eak?key=6601e332ac1abfbc3d0cb6070b4b917a'
   
  
 ];
