5e57133b65703a35912f175c3201b01319f76ec5
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-02-01 09:56:15 +0800

    Update and rename ansari1.js to ansari11.js

---
+++ ansari11.js
@@ -0,0 +1,17 @@
+var dataUrl = [
+  
+  'https://themeillogical.com/j1rxj172?key=adc36aa55f17d2c41223d390f5d7be1e',
+  'https://themeillogical.com/xyhh9ebt?key=70bf1cf20c9c44d1a9b9079b1159855b',
+  'https://dimensionuptown.com/ftevmc9ffu?key=88b2c5bdae56a3d0c808da01c98c12d7',
+  'https://dimensionuptown.com/p9m1gmpd?key=a3e88c6c6c485f3d3b80e2e54d76497a'
+  
+ 
+
+  
+  
+  
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
+
