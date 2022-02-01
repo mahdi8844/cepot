b1b9202de1e05a4cee32ce6498785066169f16e0
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-02-01 10:02:25 +0800

    Update and rename ansari2.js to ansari12.js

---
+++ ansari12.js
@@ -0,0 +1,17 @@
+var dataUrl = [
+  
+  'https://angularconstitution.com/j1xw0x0rcx?key=1c9adcc6bca3296fac552e8c1e5b0a82',
+  'https://angularconstitution.com/jpp55717e?key=8507cef1a397afc5fea1dc360c0886b3',
+  'https://fireworkswad.com/nx1jfdzr?key=8a9b83430981254c00c2804ddb2b7d92',
+  'https://fireworkswad.com/yevb1vpg2?key=0127f56241a83ccd37506a3d59a797fe'
+
+ 
+
+  
+  
+  
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
+
