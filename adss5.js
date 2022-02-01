88d4a983c3955b29f4255e34e52c01413d175da7
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-02-01 09:39:21 +0800

    Update and rename ads5.js to adss5.js

--- adss5.js
+++ adss5.js
@@ -1,8 +1,8 @@
 var dataUrl = [
   
-  'https://angularconstitution.com/qk9frn3aqu?key=1ba1f97641cdb9a5cd30010a176e827c',
+  
   'https://angularconstitution.com/g1mt10eak?key=6601e332ac1abfbc3d0cb6070b4b917a',
-  'https://angularconstitution.com/m6dshe0gb?key=6282fcaa1838cde42864f768f18a8723'
+  'https://www.effectivegatetocontent.com/mjica607za?key=2e7bf748dd3e7d803a90bbd32fd95f54'
   
  
 ];
