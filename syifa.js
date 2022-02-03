e85f6d2c4d382171e48e43b780efbc218ea08976
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-02-03 16:20:06 +0800

    Update syifa.js

--- syifa.js
+++ syifa.js
@@ -1,7 +1,9 @@
 var dataUrl = [
   
   'https://www.effectivegatetocontent.com/bq60261s?key=4268643d1293b7a87dda445d6f988ece',
-  'https://www.effectivegatetocontent.com/xyhh9ebt?key=70bf1cf20c9c44d1a9b9079b1159855b'
+  'https://www.effectivegatetocontent.com/xyhh9ebt?key=70bf1cf20c9c44d1a9b9079b1159855b',
+  'https://benoopto.com/4/4842012',
+  'https://chooxaur.com/4/4842009'
 
   
   
