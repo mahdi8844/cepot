1b44ac5870ffd215710da6f491c5b167b71181f0
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-02-18 17:33:08 +0800

    Update amat1.js

--- amat1.js
+++ amat1.js
@@ -1,14 +1,8 @@
 var dataUrl = [
   
 
-'https://angularconstitution.com/bzp6w4f9?key=1c51dd0fff2207ac667bc42f8b0de48b',
-'https://angularconstitution.com/tiiae97v?key=a7b4d96df0730d539d59907332df677c',
-'https://angularconstitution.com/cdzn7cbm?key=b2aeba2037ff43b37374d1d9fcccc12c',
-'https://angularconstitution.com/r4bk0gyx2?key=976dfcfb39bde217438881457e852721',
 'https://angularconstitution.com/dnj3chpn?key=40b634377d7d3733568f5dc8a9d01695',
-'https://angularconstitution.com/kt48cu4q?key=ccc9c339ae2b4ff0886f4b788cdea7d3',
-'https://angularconstitution.com/nz91fa4v?key=1a2e7d9384e696bed678861159a8179a'
-
+'https://angularconstitution.com/kt48cu4q?key=ccc9c339ae2b4ff0886f4b788cdea7d3'
  
   
   
