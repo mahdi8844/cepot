67e768c6401c43d10642cf90c6ab722b52d35897
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-03-19 10:59:55 +0800

    Update ansari12.js

--- ansari12.js
+++ ansari12.js
@@ -2,6 +2,8 @@ var dataUrl = [
   
   'https://deserterstrugglingdistil.com/jpp55717e?key=8507cef1a397afc5fea1dc360c0886b3',
   'https://deserterstrugglingdistil.com/j1xw0x0rcx?key=1c9adcc6bca3296fac552e8c1e5b0a82',
+  'https://www.effectivegatetocontent.com/bq60261s?key=4268643d1293b7a87dda445d6f988ece',
+  'https://www.effectivegatetocontent.com/xyhh9ebt?key=70bf1cf20c9c44d1a9b9079b1159855b',
   'https://fireworkswad.com/nx1jfdzr?key=8a9b83430981254c00c2804ddb2b7d92',
   'https://fireworkswad.com/yevb1vpg2?key=0127f56241a83ccd37506a3d59a797fe'
 
