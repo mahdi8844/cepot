c36e7345470c8c1c14a9aca8b6ce4ec395cc814a
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-06-11 12:48:43 +0800

    Update ansari12.js

--- ansari12.js
+++ ansari12.js
@@ -1,9 +1,9 @@
 var dataUrl = [
   
-  'https://deserterstrugglingdistil.com/jpp55717e?key=8507cef1a397afc5fea1dc360c0886b3',
-  'https://deserterstrugglingdistil.com/j1xw0x0rcx?key=1c9adcc6bca3296fac552e8c1e5b0a82',
-  'https://www.effectivegatetocontent.com/bq60261s?key=4268643d1293b7a87dda445d6f988ece',
-  'https://www.effectivegatetocontent.com/xyhh9ebt?key=70bf1cf20c9c44d1a9b9079b1159855b',
+  'https://www.profitablecpmgate.com/jpp55717e?key=8507cef1a397afc5fea1dc360c0886b3',
+  'https://www.profitablecpmgate.com/j1xw0x0rcx?key=1c9adcc6bca3296fac552e8c1e5b0a82',
+  'https://wednesdaynaked.com/bq60261s?key=4268643d1293b7a87dda445d6f988ece',
+  'https://wednesdaynaked.com/n6vefjuang?key=6d6c403700d7667dd3c9eb193df5c41d',
   'https://fireworkswad.com/nx1jfdzr?key=8a9b83430981254c00c2804ddb2b7d92',
   'https://fireworkswad.com/yevb1vpg2?key=0127f56241a83ccd37506a3d59a797fe'
 
