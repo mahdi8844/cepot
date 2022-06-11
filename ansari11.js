cb014c9bf6537dfe1823490f382a60fc0328cbba
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-06-11 12:35:12 +0800

    Update ansari11.js

--- ansari11.js
+++ ansari11.js
@@ -4,8 +4,8 @@ var dataUrl = [
   'https://themeillogical.com/xyhh9ebt?key=70bf1cf20c9c44d1a9b9079b1159855b',
   'https://grantedpigsunborn.com/mmj2k7nrdt?key=a9adec45b7e8413e8085337ca17f76bf',
   'https://grantedpigsunborn.com/eun6bc7z?key=7eb35efd04314c17bed2c3b7411852d9',
-  'https://dimensionuptown.com/ftevmc9ffu?key=88b2c5bdae56a3d0c808da01c98c12d7',
-  'https://dimensionuptown.com/p9m1gmpd?key=a3e88c6c6c485f3d3b80e2e54d76497a'
+  'https://amendablepredicamentvertical.com/ftevmc9ffu?key=88b2c5bdae56a3d0c808da01c98c12d7',
+  'https://amendablepredicamentvertical.com/p9m1gmpd?key=a3e88c6c6c485f3d3b80e2e54d76497a'
   
  
 
