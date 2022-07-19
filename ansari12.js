6e3536b873a9e67b4d8385dcf74ac1af875e75af
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-07-19 19:25:21 +0800

    Update ansari12.js

--- ansari12.js
+++ ansari12.js
@@ -1,7 +1,7 @@
 var dataUrl = [
   
-  'https://www.profitablecpmgate.com/jpp55717e?key=8507cef1a397afc5fea1dc360c0886b3',
-  'https://www.profitablecpmgate.com/j1xw0x0rcx?key=1c9adcc6bca3296fac552e8c1e5b0a82',
+  'https://www.highperformancegate.com/jpp55717e?key=8507cef1a397afc5fea1dc360c0886b3',
+  'https://www.highperformancegate.com/j1xw0x0rcx?key=1c9adcc6bca3296fac552e8c1e5b0a82',
   'https://wednesdaynaked.com/bq60261s?key=4268643d1293b7a87dda445d6f988ece',
   'https://wednesdaynaked.com/n6vefjuang?key=6d6c403700d7667dd3c9eb193df5c41d',
   'https://fireworkswad.com/nx1jfdzr?key=8a9b83430981254c00c2804ddb2b7d92',
