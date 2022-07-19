bda73a0632a08716e8d3b5510a3dbc3ccc1d65a1
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-07-19 19:07:33 +0800

    Update adsss4.js

--- adsss4.js
+++ adsss4.js
@@ -1,7 +1,7 @@
 var dataUrl = [
   
   'https://annoynoveltyeel.com/ig04kr58g?key=5ee5e1490ffbfafaf25fdf6a0ba7a6a9',
-  'https://www.profitablecpmgate.com/w60j252h?key=7d2a8ef66871f5d2dc6faa1821ff582e'
+  'https://www.highperformancegate.com/w60j252h?key=7d2a8ef66871f5d2dc6faa1821ff582e'
   
  
 ];
