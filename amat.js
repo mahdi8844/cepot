35e59983da0c55eb1f80039d793ff8b3e9d94672
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-07-19 18:19:33 +0800

    Update amat.js

--- amat.js
+++ amat.js
@@ -3,7 +3,7 @@ var dataUrl = [
 'https://www.highperformancegate.com/kt48cu4q?key=ccc9c339ae2b4ff0886f4b788cdea7d3',
 'https://www.highperformancegate.com/dnj3chpn?key=40b634377d7d3733568f5dc8a9d01695',
 'https://www.highperformancegate.com/s14rtjzf87?key=7084627f2c1bd80cb438286db65a05e1',
-'https://www.highperformancegate.com/r4bk0gyx2?key=976dfcfb39bde217438881457e852721',
+'https://www.highperformancegate.com/r4bk0gyx2?key=976dfcfb39bde217438881457e852721'
  
  
   
