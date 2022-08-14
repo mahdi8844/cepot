1ff1d2e5212df1bc61cb1ef53dd2ae64d47f37e2
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-08-14 19:16:49 +0800

    Update adsss4.js

--- adsss4.js
+++ adsss4.js
@@ -1,15 +1,8 @@
 var dataUrl = [
   
-  'https://annoynoveltyeel.com/ig04kr58g?key=5ee5e1490ffbfafaf25fdf6a0ba7a6a9',
-  'https://annoynoveltyeel.com/bk6xziud?key=24a93215ae3fd40b02e4035bb344aa7b',
-  'https://annoynoveltyeel.com/w8jnjvgd8?key=93be26cc598d7b822d996f8d7e239758',
-  'https://annoynoveltyeel.com/s18bncwz?key=d185a86de0729a4f4e0943ca13d6d8ee',
-  'https://annoynoveltyeel.com/wbbzz1u91r?key=d0ccda3580917dc6f6247f5013028171',
-  'https://www.highperformancegate.com/g1mt10eak?key=6601e332ac1abfbc3d0cb6070b4b917a',
-  'https://www.highperformancegate.com/m6dshe0gb?key=6282fcaa1838cde42864f768f18a8723',
-  'https://www.highperformancegate.com/qk9frn3aqu?key=1ba1f97641cdb9a5cd30010a176e827c',
-  'https://www.highperformancegate.com/iq7nj3dp?key=50c38db2ae189bc547ea25bb6d7a95ff',
-  'https://www.highperformancegate.com/w60j252h?key=7d2a8ef66871f5d2dc6faa1821ff582e'
+
+ 'https://annoynoveltyeel.com/wbbzz1u91r?key=d0ccda3580917dc6f6247f5013028171',
+ 'https://www.highperformancegate.com/w60j252h?key=7d2a8ef66871f5d2dc6faa1821ff582e'
   
  
 ];
