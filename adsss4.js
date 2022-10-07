d9b8e8b123f71732ccd37bd4aa8eb8228f278520
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-10-07 09:41:30 +0800

    Update adsss4.js

--- adsss4.js
+++ adsss4.js
@@ -1,8 +1,9 @@
 var dataUrl = [
   
 
- 'https://annoynoveltyeel.com/wbbzz1u91r?key=d0ccda3580917dc6f6247f5013028171',
- 'https://www.highperformancegate.com/w60j252h?key=7d2a8ef66871f5d2dc6faa1821ff582e'
+ 'https://www.profitablegatetocontent.com/w11f6fh2ua?key=aeffd39951a7f32c2bcf38fc8a4ae4d8',
+ 'https://www.profitablegatetocontent.com/w60j252h?key=7d2a8ef66871f5d2dc6faa1821ff582e',
+ 'https://www.profitablegatetocontent.com/wbbzz1u91r?key=d0ccda3580917dc6f6247f5013028171'
   
  
 ];
