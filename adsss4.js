ab25a0857d5c02c65e37b67890150ca69ba461db
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2023-06-22 13:07:00 +0800

    Update adsss4.js

--- adsss4.js
+++ adsss4.js
@@ -1,7 +1,9 @@
 var dataUrl = [
   
 
- 'https://www.highrevenuegate.com/hx3g9hvzd?key=8ffd8310adecd93c5ec27d2ed8e75016',
+'https://www.highrevenuegate.com/hx3g9hvzd?key=8ffd8310adecd93c5ec27d2ed8e75016',
+'https://www.highrevenuegate.com/mr8kenhu8r?key=db7b9e6af3a247db6cc5ea2058829856',
+'https://www.highrevenuegate.com/uyh47i57?key=521dbcfa9c33f09f9006287ca4487e30',
 'https://www.highrevenuegate.com/dc4j91c7v?key=9b2445d7fd2d084d5beb8e3db8d52fd5'
   
  
