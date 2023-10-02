568d91208e08a6a844da5af3c1411d323fcafc80
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2023-10-02 13:59:47 +0800

    Update adsss4.js

--- adsss4.js
+++ adsss4.js
@@ -1,10 +1,10 @@
 var dataUrl = [
   
 
-'https://www.highcpmrevenuegate.com/uyh47i57?key=521dbcfa9c33f09f9006287ca4487e30',
-'https://www.highcpmrevenuegate.com/mr8kenhu8r?key=db7b9e6af3a247db6cc5ea2058829856',
-'https://www.highcpmrevenuegate.com/hx3g9hvzd?key=8ffd8310adecd93c5ec27d2ed8e75016',
-'https://www.highcpmrevenuegate.com/dc4j91c7v?key=9b2445d7fd2d084d5beb8e3db8d52fd5'
+'https://zaimovie.com/en/movie',
+'https://zaimovie.com/en/movie',
+'https://zaimovie.com/en/movie',
+'https://zaimovie.com/en/movie'
   
  
 ];
