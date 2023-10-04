0c37a91e3646521dd6f36c12786aec2701b1816b
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2023-10-04 10:44:17 +0800

    Update adss5.js

--- adss5.js
+++ adss5.js
@@ -1,8 +1,7 @@
 var dataUrl = [
   
-  
-  'https://annoynoveltyeel.com/wbbzz1u91r?key=d0ccda3580917dc6f6247f5013028171',
-  'https://annoynoveltyeel.com/bk6xziud?key=24a93215ae3fd40b02e4035bb344aa7b'
+
+'https://zaimovie.com/en/movie'
   
  
 ];
