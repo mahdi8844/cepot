cea01a99d610d22c982cad169303583d340f8d47
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2023-10-17 15:11:09 +0800

    Update ads4.js

--- ads4.js
+++ ads4.js
@@ -1,9 +1,7 @@
 var dataUrl = [
   
-  'https://www.effectivegatetocontent.com/z1wn7uchfh?key=8e7e9cee0a6e0c3072355923040687d0',
-  'https://www.effectivegatetocontent.com/sjkfhz8m3?key=db7f349f9a05ee1b03fb0c88a9657693',
-  'https://www.effectivegatetocontent.com/w11f6fh2ua?key=aeffd39951a7f32c2bcf38fc8a4ae4d8'
-  
+  'https://r7movies.com/en/movie'
+
  
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
